9fe159365cf53eec13ae59e1096bc473bc45111a
viveksavita <viveksavita@gmail.com>
2026-01-06 00:29:39 +0000

    Passing parameter in path

--- chapter_1/app.py
+++ chapter_1/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
+from enum import Enum
 
 class Settings(BaseSettings):
     app_name: str = "My FastAPI App"
@@ -8,6 +9,13 @@ class Settings(BaseSettings):
     items_per_user: int = 50
 
 settings = Settings()
+
+# Example class for enumeration in path
+
+class ModelName(str, Enum) :
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
@@ -19,3 +27,18 @@ async def info():
         "admin_email": settings.admin_email,
         "items_per_user": settings.items_per_user,
     }
+
+@app.get("/custom/{your_name}")
+async def custom(your_name: str):
+    return {
+        "Name:": your_name
+    }
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
