e995ff19ef5a692b7ad41a9da4b4acbcaa64de80
viveksavita <viveksavita@gmail.com>
2026-01-15 23:36:57 +0000

    computed fiedl example

--- pydantic_learn/lesson1.py
+++ pydantic_learn/lesson1.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone, UTC
 from uuid import UUID, uuid4
 from typing import Literal, Annotated
 from functools import partial # allow us to pass arguments to default_factory functions
-from pydantic import BaseModel, Field, EmailStr, HttpUrl, ValidationError, SecretStr, field_validator, model_validator
+from pydantic import BaseModel, Field, EmailStr, HttpUrl, ValidationError, SecretStr, field_validator, model_validator, computed_field
 
 
 class User(BaseModel):
@@ -12,7 +12,9 @@ class User(BaseModel):
     Email:str
     bio:str = ""
     is_active:bool = True
-    full_name:str | None = None
+    first_name:str | None = None
+    last_name:str | None = None
+    followers_count:int = 0
 
     @field_validator('username')
     @classmethod
@@ -20,12 +22,24 @@ class User(BaseModel):
         if not v.replace("_","").isalnum():
             raise ValueError("username must be alphanumeric with underscores allowed")
         return v.lower()
+    
+    @computed_field
+    @property
+    def display_name(self) -> str:
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.username
 
+    @computed_field
+    @property
+    def is_influencer(self) -> bool:
+        return (self.followers_count or 0) > 1000
 
 user = User(
     uid=1,
     username="dhaanush_VIEK_99",
-    Email ="dhaanush@gmail.com",)
+    Email ="dhaanush@gmail.com",
+    followers_count=10000000)
 
 
 # example to see the output of the class model
@@ -104,3 +118,8 @@ try:
     )
 except ValidationError as e:
     print(e.json())
+
+
+# Example of computed fields
+
+
